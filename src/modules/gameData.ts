f52e2be159090f24ae15134d89122ec9e3cc7c87
RyoKojima <ryokojima7@gmail.com>
2019-09-23 09:32:44 +0900

    Add gamedata

---
+++ src/modules/gameData.ts
@@ -0,0 +1,10 @@
+export class GameData {
+    score: number        = 0
+    count: number        = 9
+    isStraight: Boolean  = false
+    isRight: Boolean     = false
+    isDown: Boolean      = false
+    canStraight: Boolean = false
+    canRight: Boolean    = false
+    canDown: Boolean     = false
+}
