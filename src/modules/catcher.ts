7c2ca40bffd397f7b242f6e41faf99dca2b1d2db
RyoKojima <ryokojima7@gmail.com>
2019-09-23 09:33:23 +0900

    Fix logic for handle catcher

--- src/modules/catcher.ts
+++ src/modules/catcher.ts
@@ -1,3 +1,7 @@
+import { GameData } from "./gameData";
+
+const SPEED = 0.4
+
 export function spawnCatcher() {
     const catcher = new Entity()
     catcher.addComponent(new ConeShape())
@@ -10,4 +14,106 @@ export function spawnCatcher() {
     catcher.getComponent(Material).albedoColor = Color3.Red()
 
     return catcher
+}
+
+export function spawnStraightBtn (gameData) {
+    const straightBtn  = new Entity()
+    straightBtn.addComponent(new PlaneShape())
+
+    const transform = new Transform()
+    transform.position.set(2, 1, 3)
+    straightBtn.addComponent(transform)
+    
+    straightBtn.addComponent(new Material())
+    straightBtn.getComponent(Material).albedoColor = Color3.Teal()
+
+    const label = new TextShape("Straight")
+    label.fontSize = 2
+    label.zIndex = 5
+    straightBtn.addComponent(label);
+
+    straightBtn.addComponent(
+        new OnPointerDown(e => {
+            if (gameData.canStraight && gameData.count !== 0) {
+                if (gameData.isStraight) {
+                    gameData.isStraight  = false
+                    gameData.canStraight = false
+                    gameData.canRight    = true
+                    straightBtn.getComponent(TextShape).value = "Straight"
+                } else {
+                    straightBtn.getComponent(TextShape).value = "Stop"
+                    gameData.isStraight  = true
+                    gameData.count--
+                }
+            } 
+        })
+    )
+    return straightBtn
+}
+
+export function spawnRightBtn(gameData) {
+    const rightBtn = new Entity()
+    rightBtn.addComponent(new PlaneShape())
+
+    const transform = new Transform()
+    transform.position.set(4, 1, 3)
+    rightBtn.addComponent(transform)
+    
+    rightBtn.addComponent(new Material())
+    rightBtn.getComponent(Material).albedoColor = Color3.Magenta()
+
+    const label = new TextShape("Right")
+    label.fontSize = 2
+    label.zIndex = 5
+    rightBtn.addComponent(label);
+
+    rightBtn.addComponent(
+        new OnPointerDown(e => {
+            if(gameData.canRight && gameData.count !== 0) {
+                if (gameData.isRight) {
+                    gameData.isRight  = false
+                    gameData.canRight = false
+                    gameData.canDown  = true
+                    rightBtn.getComponent(TextShape).value = "Right"
+                } else {
+                    rightBtn.getComponent(TextShape).value = "Stop"
+                    gameData.isRight  = true
+                    gameData.count--
+                }
+            }
+        })
+    )
+    return rightBtn
+}
+
+export class MoveForward {
+    game: GameData
+    catcher: IEntity
+    constructor(gameData, catcher) {
+        this.game    = gameData
+        this.catcher = catcher
+    }
+    update() {
+        if (this.game.isStraight && this.game.canStraight) {
+            let transform = this.catcher.getComponent(Transform)
+            let distance = Vector3.Forward().scale(SPEED)
+            transform.translate(distance)
+        }
+    }
+}
+
+export class MoveRight {
+    game: GameData
+    catcher: IEntity
+    constructor(gameData, catcher) {
+        this.game    = gameData
+        this.catcher = catcher
+    }
+    update() {
+        if (this.game.isRight && this.game.canRight) {
+            let transform = this.catcher.getComponent(Transform)
+            let distance = Vector3.Right().scale(SPEED)
+            transform.translate(distance)
+        }
+    }
 }
---
+++ src/modules/downBtn.ts
@@ -0,0 +1,87 @@
+import utils from "../../node_modules/decentraland-ecs-utils/index"
+import { items, ItemData } from "./items";
+import { GameData } from "./gameData";
+
+export function spawnDownBtn(gameData, catcher) {
+    const catchBtn = new Entity()
+    catchBtn.addComponent(new PlaneShape())
+
+    const transform = new Transform()
+    transform.position.set(6, 1, 3)
+    catchBtn.addComponent(transform)
+    
+    catchBtn.addComponent(new Material())
+    catchBtn.getComponent(Material).albedoColor = Color3.Magenta()
+
+    const label = new TextShape("Down")
+    label.fontSize = 2
+    label.zIndex = 5
+    catchBtn.addComponent(label);
+
+    catchBtn.addComponent(
+        new OnPointerDown(e => {
+            if (gameData.canDown && gameData.count !== 0) {
+                if (gameData.isDown) {
+                    returnCatcher(catcher, gameData, catchBtn)
+                } else {
+                    gameData.isDown = true
+                    gameData.count--
+
+                    catchBtn.getComponent(TextShape).value = "Return"
+                }
+            } else if(gameData.count === 0) {
+                returnCatcher(catcher, gameData, catchBtn)
+            }
+        })
+    )
+    return catchBtn
+}
+
+export class Down {
+    catcher: IEntity
+    game: GameData
+    scoreLabel: TextShape
+
+    constructor(catcher, gameData, scoreLabel) {
+        this.catcher    = catcher,
+        this.game       = gameData,
+        this.scoreLabel = scoreLabel 
+    }
+    update() {
+        let catcherY = this.catcher.getComponent(Transform).position.y
+        if (this.game.isDown && this.game.canDown) {
+            if (catcherY > 0.5) {
+                let transform = this.catcher.getComponent(Transform)
+                let distance = Vector3.Down().scale(1)
+                transform.translate(distance)
+                removeEntity(this.scoreLabel, this.game);
+            }
+        }
+    }
+}
+
+function removeEntity(scoreLabel, gameData) {
+    for (let item of items.entities) {
+        if (item.getComponent(ItemData).isHit) {
+            updateScore(scoreLabel, item.getComponent(ItemData).score, gameData);
+            engine.removeEntity(item)
+        }
+    }
+}
+
+function updateScore(scoreLabel, score, gameData) {
+    gameData.score += score 
+    scoreLabel.value = gameData.score.toString()
+}
+
+function returnCatcher(catcher, gameData, catchBtn) {
+    gameData.isDown  = false
+    gameData.canDown = false
+
+    let startPos = catcher.getComponent(Transform).position
+    let endPos = new Vector3(2, 3, 5)
+    catcher.addComponent(new utils.MoveTransformComponent(startPos, endPos, 1, () => {
+        gameData.canStraight = true
+        catchBtn.getComponent(TextShape).value = "Down"
+    }))
+}
